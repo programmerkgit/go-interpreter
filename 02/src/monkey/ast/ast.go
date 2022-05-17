32d2c8e8df6d195f38a705826e28748012e8d8c8
keishi kubo <37021071+programmerkgit@users.noreply.github.com>
2022-05-18 01:56:51 +0900

    call expression

--- 02/src/monkey/ast/ast.go
+++ 02/src/monkey/ast/ast.go
@@ -274,3 +274,30 @@ func (f FunctionLiteral) expressionNode() {
 	//TODO implement me
 	panic("implement me")
 }
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (c *CallExpression) TokenLiteral() string {
+	return c.Token.Literal
+}
+
+func (c *CallExpression) String() string {
+	var out bytes.Buffer
+
+	var args []string
+	for _, a := range c.Arguments {
+		args = append(args, a.String())
+	}
+	out.WriteString(c.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ","))
+	out.WriteString(")")
+	return out.String()
+}
+
+func (c *CallExpression) expressionNode() {
+}
